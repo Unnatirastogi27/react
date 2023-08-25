ade34e2e8444148edbfadd1cb000cb9e86a6fffa
Unnatirastogi27 <110484749+Unnatirastogi27@users.noreply.github.com>
2023-08-25 15:35:34 +0530

    Create EventEx.jsx

---
+++ EventEx.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChangeName from './ChangeName'
+
+const EventEx = () => {
+    
+  return (
+    <div>
+        <h2>Event Handling Example</h2>
+        <ChangeName/>
+    </div>
+  )
+}
+
+export default EventEx
